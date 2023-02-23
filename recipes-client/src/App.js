b5a93c7fbf1ecd27ef5a61b8248d41798d773bc4
Waleed <wsaleh1991@gmail.com>
2023-02-22 22:47:01 -0600

    Created boiler plate for layout of each recipe

--- recipes-client/src/App.js
+++ recipes-client/src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import api from "./api/axiosconfig";
+import { Card } from "react-bootstrap";
 
 function App() {
-	const [recipes, setRecipes] = useState(null);
+	const [recipes, setRecipes] = useState([]);
 
 	const fetchRecipes = async () => {
 		try {
 			const resp = await api.get("/api/v1/recipes");
-			console.log(resp.data);
+			setRecipes(resp.data);
 		} catch (error) {
 			console.log(error);
 		}
@@ -18,9 +19,28 @@ function App() {
 		fetchRecipes();
 	}, []);
 
+	console.log(recipes[1]);
+
 	return (
 		<div className='App'>
-			<h1></h1>
+			{recipes.map((recipe, i) => (
+				<Card key={i} style={{ width: "18rem" }}>
+					<Card.Img variant='top' src={recipe.recipeImage} />
+					<Card.Body>
+						<Card.Title>{recipe.recipeName}</Card.Title>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the
+							bulk of the card's content.
+						</Card.Text>
+						<Card.Text>Cook Time: {recipe.cookTime}</Card.Text>
+						<Card.Text>Prep Time: {recipe.prepTime}</Card.Text>
+						<Card.Text>Ingredients:</Card.Text>
+						{recipe.ingredients.map((ing, index) => (
+							<span key={index}>{(index ? ", " : "") + ing}</span>
+						))}
+					</Card.Body>
+				</Card>
+			))}
 		</div>
 	);
 }
